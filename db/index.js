312ad2cd68f2771f8f39a7d7d36cef2b99c6952e
Jean-Luc <103611121+jltp2c@users.noreply.github.com>
2023-04-02 18:35:44 +0200

    last commit tekko

--- db/index.js
+++ db/index.js
@@ -6,7 +6,11 @@ const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/Tekko";
 
 async function openConnection() {
   try {
-    return await mongoose.set("strictQuery", true).connect(MONGO_URI);
+    return await mongoose
+      .set("strictQuery", true)
+      .connect(
+        "mongodb+srv://jltp2c:YAU7SbSQTT6TMUtx@tekko-appfinal.fzajj40.mongodb.net/?retryWrites=true&w=majority"
+      );
   } catch (error) {
     console.error(`Error while connecting to the database: ${error.message}`);
   }
